ac0742dc6fad36cb387a4736468cf2718d79906a
yand1960@gmail.com <yand@outlook.com>
2021-08-09 20:04:43 +0300

    typos

--- JA_01_Lambda/src/my/lambdas/IntegerCollectionManager.java
+++ JA_01_Lambda/src/my/lambdas/IntegerCollectionManager.java
@@ -3,7 +3,7 @@ package my.lambdas;
 import java.util.ArrayList;
 import java.util.List;
 
-// "Классический" класс, который принимает данные в качестве аргрументов
+// "Классический" класс, который принимает данные в качестве аргументов
 public class IntegerCollectionManager {
 
     public List<Integer> squares(List<Integer> data) {
--- JA_01_Lambda/src/my/lambdas/TransformInteger.java
+++ JA_01_Lambda/src/my/lambdas/TransformInteger.java
@@ -1,5 +1,7 @@
 package my.lambdas;
 
+//Мой собственный функциональный интерфейс
+
 @FunctionalInterface
 public interface TransformInteger {
     public Integer doTransform(Integer x);
