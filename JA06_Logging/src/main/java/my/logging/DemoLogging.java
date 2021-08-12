818e27724a1b663f34ab060273490400a1bf59c3
yand1960@gmail.com <yand@outlook.com>
2021-08-12 14:55:57 +0300

    trainer

---
+++ JA06_Logging/src/main/java/my/logging/DemoLogging.java
@@ -0,0 +1,19 @@
+package my.logging;
+
+import java.io.IOException;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+public class DemoLogging {
+    public static void main(String[] args) throws IOException {
+        LogManager
+                .getLogManager()
+                .readConfiguration(
+                        DemoLogging.class.getResourceAsStream("logger.properties")
+                );
+
+        Logger logger = Logger.getLogger(DemoLogging.class.getName());
+        logger.info("It is info");
+        logger.severe("I am severe");
+    }
+}
