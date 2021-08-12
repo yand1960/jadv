bfa313558640bd7e5ddb829f472203b1306521a2
yand1960@gmail.com <yand@outlook.com>
2021-08-12 15:54:51 +0300

    trainer

---
+++ JA_07_Swing/src/main/java/my/swing/HelloWorld.java
@@ -0,0 +1,33 @@
+package my.swing;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class HelloWorld extends JFrame {
+
+    JButton button = new JButton();
+    JTextField text1 = new JTextField();
+
+    public HelloWorld() {
+        //this.setLayout(new GridLayout(2,1));
+        this.setLayout(null);
+        button.setText("Click me!");
+        button.setBounds(5,40,100,40);
+        button.addActionListener(e -> {
+//            button.setText("Hello, world!");
+            button.setText("Hello, " + text1.getText());
+        });
+
+        text1.setBounds(5,5,100,20);
+        this.add(text1);
+        this.add(button);
+        this.setBounds(20,20,140,120);
+
+    }
+
+    public static void main(String[] args) {
+        HelloWorld frame = new HelloWorld();
+        frame.setVisible(true);
+    }
+
+}
